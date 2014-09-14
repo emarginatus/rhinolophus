1fc3c3698c666b7e16f42e5c1393b0067009566a
ThierryO <ggplot@muscardinus.be>
2014-09-14 23:13:28 +0200

    add time expansion factor to read.wav

--- R/read_wav.R
+++ R/read_wav.R
@@ -1,9 +1,10 @@
 #' read a WAV file
 #' @param filename The name of the file
 #' @param channel Select the left or the right channel
+#' @param time.expansion.factor The factor to which the original sound was slowed down prior to recording
 #' @export
 #' @importFrom tuneR readWave
-read.wav <- function(filename, channel = c("left", "right")){
+read.wav <- function(filename, channel = c("left", "right"), time.expansion.factor = 10){
   channel <- match.arg(channel)
   header <- readWave(filename, header = TRUE)
   raw.data <- readWave(filename)
@@ -14,7 +15,7 @@ read.wav <- function(filename, channel = c("left", "right")){
   }
   return(
     list(
-      sample.rate = header$sample.rate,
+      sample.rate = header$sample.rate * time.expansion.factor,
       sample = header$samples,
       values = selected.channel
     )

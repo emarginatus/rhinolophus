d6f159fe95f0d479f387f92b52b3ea9811a9414b
ThierryO <ggplot@muscardinus.be>
2014-10-02 20:36:38 +0200

    rename plot.pulse to reconstruct.pulse

--- R/reconstruct_pulse.R
+++ R/reconstruct_pulse.R
@@ -4,20 +4,21 @@
 #' @param local.time Should the pulse be centered (TRUE) or display the original timing (FALSE)
 #' @param plot.it Should the reconstructed pulse be plotted?
 #' @importFrom raster raster
-plot.pulse <- function(pulse.fft, local.time = TRUE, plot.it = TRUE){
+reconstruct.pulse <- function(pulse.fft, local.time = TRUE, plot.it = TRUE){
   fft.coefs <- matrix(0, nrow = pulse.fft$fft.dim[1], ncol = pulse.fft$fft.dim[2])
-  fft.coefs[seq_len(nrow(pulse.fft$fft)), seq_len(ncol(pulse.fft$fft))] <- pulse.fft$fft
+  max.dim <- pmin(dim(pulse.fft$fft), pulse.fft$fft.dim)
+  fft.coefs[seq_len(max.dim[1]), seq_len(max.dim[2])] <- pulse.fft$fft[seq_len(max.dim[1]), seq_len(max.dim[2])]
   amplitude <- abs(fft(fft.coefs, inverse = TRUE) / prod(pulse.fft$fft.dim))
-  extra.time <- ifelse(local.time, 0, pulse.fft$start.time) / 1000
+  extra.time <- ifelse(local.time, 0, pulse.fft$start.time)
   puls.raster <- raster(
     amplitude[rev(seq_len(pulse.fft$fft.dim[1])), ],
     xmn = extra.time,
-    xmx = extra.time + pulse.fft$puls.duration / 1000,
+    xmx = extra.time + pulse.fft$puls.duration,
     ymn = pulse.fft$frequency.range[1],
     ymx = pulse.fft$frequency.range[2]
   )
   if(plot.it){
-    plot(puls.raster, xlab = 'Time (ms)', ylab = "Frequency (kHz)", asp = 1e-3)
+    plot(puls.raster, xlab = 'Time (ms)', ylab = "Frequency (kHz)", asp = 1)
   }
   return(invisible(puls.raster))
 }

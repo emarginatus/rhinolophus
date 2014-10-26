7fa02fabebd53ebac4b8aa3aa4eab2c2945503d1
ThierryO <ggplot@muscardinus.be>
2014-10-26 16:38:59 +0100

    add ensemble.predict

---
+++ R/ensemble_predict.R
@@ -0,0 +1,13 @@
+#' Get predicted probabilities of class membership based on an ensemble of Kohonen self organizing maps
+#' @export
+#' @param truth the known classification
+#' @param pattern the patterns of the known classification
+#' @param newdata the pattern to get predictions for
+#' @param n.ensemble Number of kohonen SOM's to use
+ensemble.predict <- function(truth, pattern, newdata, n.ensemble = 10){
+  predictions <- lapply(seq_len(n.ensemble), function(i){
+    model <- trainrda(pattern, truth)
+    predict(model, newdata = newdata)$prediction
+  })
+  Reduce("+", predictions) / n.ensemble
+}

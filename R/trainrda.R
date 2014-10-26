ddd3581a3e39113796ac91d5c73bba34335092f3
ThierryO <ggplot@muscardinus.be>
2014-10-26 16:23:37 +0100

    add the trainrda function

---
+++ R/trainrda.R
@@ -0,0 +1,20 @@
+#' Train a Kohonen network
+#' @export
+#' @importFrom kohonen xyf
+#' @importFrom class somgrid
+#' @param pattern A matrix with patterns
+#' @param truth A data.frame with the truth
+trainrda <- function(pattern, truth){
+  truth$combinations <- paste(truth$type, truth$species, sep = ":")
+  types <- table(truth$combinations)
+  truth$combinations[truth$combinations %in% names(types)[types < 10]] <- "other:"
+  Y <- model.matrix(~ 0 + combinations, data = truth)
+  Y <- Y[, colnames(Y) != "combinationsother:"]
+  colnames(Y) <- names(types)[types >= 10]
+  n.dim <- floor(log(nrow(truth)))
+  xyf(
+    pattern,
+    Y,
+    somgrid(n.dim, n.dim, "hexagonal")
+  )
+}

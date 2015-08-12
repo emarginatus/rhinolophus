f0da25d8213bae91d2281b86ab3c70de3a8ec405
ThierryO <ggplot@muscardinus.be>
2015-08-13 01:06:06 +0200

    add unit tests for spectrogram_contour()

---
+++ tests/testthat/test_spectrogram_contour.R
@@ -0,0 +1,31 @@
+context("spectrogram_contour")
+wav <- read_wav(
+  system.file("demo_wav/leislers.wav", package = "rhinolophus")
+)
+spectrogram <- wav_2_spectrogram(wav)
+expect_error(
+  spectrogram_contour("a"),
+  "spectrogram does not inherit from class specgram"
+)
+expect_error(
+  spectrogram_contour(spectrogram, contour.level = "a"),
+  "contour\\.level is not a numeric or integer vector"
+)
+expect_error(
+  spectrogram_contour(spectrogram, contour.level = 0),
+  "contour\\.level not greater than 0"
+)
+expect_error(
+  spectrogram_contour(spectrogram, contour.level = -1),
+  "contour\\.level not greater than 0"
+)
+
+expect_is(
+  spectrogram_contour(spectrogram),
+  "SpatialPolygonsDataFrame"
+)
+this.contour.level <- c(-2, 2)
+contours <- spectrogram_contour(spectrogram, contour.level = this.contour.level)
+expect_true(
+  all(contours@data$level %in% this.contour.level)
+)

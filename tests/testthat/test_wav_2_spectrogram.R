3a29604841b7cd18f34ab77e682eb228b4649a7a
ThierryO <ggplot@muscardinus.be>
2015-08-12 23:25:00 +0200

    add unit tests for wav_2_spectrogram()

---
+++ tests/testthat/test_wav_2_spectrogram.R
@@ -0,0 +1,26 @@
+context("wav_2_spectrogram")
+expect_error(wav_2_spectrogram("a"), "wav is not a list")
+expect_error(
+  wav_2_spectrogram(list(values = 1)),
+  "wav does not have name sample.rate"
+)
+expect_error(
+  wav_2_spectrogram(list(sample.rate = 1)),
+  "wav does not have name values"
+)
+expect_error(
+  wav_2_spectrogram(list(sample.rate = "a", values = 1)),
+  "wav\\$sample.rate is not a number \\(a length one numeric vector\\)"
+)
+expect_error(
+  wav_2_spectrogram(list(sample.rate = 1, values = "a")),
+  "wav\\$values is not a numeric or integer vector"
+)
+expect_error(
+  wav_2_spectrogram(list(sample.rate = 1, values = 1), window.ms = "a"),
+  "window.ms is not a number \\(a length one numeric vector\\)"
+)
+expect_error(
+  wav_2_spectrogram(list(sample.rate = 1, values = 1), window.ms = 0),
+  "window.ms not greater than 0"
+)

f49f2f841b110db5867a7b1f707cbe2559b8b273
ThierryO <ggplot@muscardinus.be>
2015-08-16 00:02:20 +0200

    update code style of wav2rda()

--- R/wav2rda.R
+++ R/wav2rda.R
@@ -1,13 +1,28 @@
 #' Find the pulses in a bunch of wav files and save them to rda files
 #' @export
-#' @param path the path of the wav files
-#' @param te.factor The time-expansion factor used in the wav files. Defaults to 10
-#' @param channel Which channel will be read. Defaults to "right"
-#' @param window.ms The size of the window for fourier transformation of the sound in milliseconds. The time-expansion factor is taken into account. Defaults to 1.
-#' @param min.contour The minimum amplitude to mark the start of a pulse. Expressed in dB above the median amplitude of the wav file.
-#' @param min.peak The required maximum amplitude of a pulse. Pulses with a maximum amplitude below min.peak will be ignored.
-#' @param n.fourier The number of parameters of the Fourier Transformation to store per dimension. Defaults to 30.
-wav2rda <- function(path, te.factor = 10, channel = "right", window.ms = 2, min.contour = 10, min.peak = 20, n.fourier = 30){
+#' @param path The path of the wav files. The function will reach recursive in
+#'    the subdirectories of the path.
+#' @param te.factor The time-expansion factor used in the wav files. Defaults
+#'    to 10.
+#' @param channel Which channel will be read. Defaults to "right".
+#' @param window.ms The size of the window for fourier transformation of the
+#'    sound in milliseconds. The time-expansion factor is taken into account.
+#'    Defaults to 1.
+#' @param min.contour The minimum amplitude to mark the start of a pulse.
+#'    Expressed in dB above the median amplitude of the wav file.
+#' @param min.peak The required maximum amplitude of a pulse. Pulses with a
+#'    maximum amplitude below min.peak will be ignored.
+#' @param n.fourier The number of parameters of the Fourier Transformation to
+#'    store per dimension. Defaults to 30.
+wav2rda <- function(
+  path,
+  te.factor = 10,
+  channel = "right",
+  window.ms = 2,
+  min.contour = 10,
+  min.peak = 20,
+  n.fourier = 30
+){
   filenames <- list.files(
     path = path,
     pattern = "\\.wav$",
@@ -15,7 +30,7 @@ wav2rda <- function(path, te.factor = 10, channel = "right", window.ms = 2, min.
     ignore.case = TRUE,
     full.names = TRUE
   )
-  for(filename in filenames){
+  for (filename in filenames) {
     message(filename)
     wav <- read_wav(
       filename = filename,
@@ -38,7 +53,11 @@ wav2rda <- function(path, te.factor = 10, channel = "right", window.ms = 2, min.
       n.fourier = n.fourier
     )
     rm(pulses)
-    save(pulses.fft, te.factor, channel, window.ms, min.contour, min.peak, n.fourier, file = gsub("(WAV|wav)$", "rda", filename))
+    save(
+      pulses.fft, te.factor, channel, window.ms, min.contour, min.peak,
+      n.fourier,
+      file = gsub("(WAV|wav)$", "rda", filename)
+    )
     rm(pulses.fft)
     gc()
   }

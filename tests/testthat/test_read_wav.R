2caf301da55fded7f7cf79e09decce566cde0b5e
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-11-08 23:04:52 +0100

    update unit tests

--- tests/testthat/test_read_wav.R
+++ tests/testthat/test_read_wav.R
@@ -2,23 +2,23 @@ context("read_wav")
 dir <- tempdir()
 expect_error(
   read_wav(filename = dir),
-  paste(dir, "does not exist")
+  paste(normalizePath(dir, winslash = "/"), "is not a file")
 )
 expect_is(
   leislers <- read_wav(
     filename = system.file("demo_wav/leislers.wav", package = "rhinolophus")
   ),
-  "list"
+  "batWav"
 )
-expect_identical(leislers$sample.rate, 441000)
-expect_identical(leislers$sample, 5065L)
-expect_identical(leislers$sample, length(leislers$values))
+expect_identical(leislers@Metadata$SamplingRate, 441000)
+expect_identical(leislers@Metadata$TimeExpansionFactor, 10L)
+expect_identical(leislers@Metadata$Channel, factor("left", c("left", "right")))
 
 expect_is(
   leislers.right <- read_wav(
     filename = system.file("demo_wav/leislers.wav", package = "rhinolophus"),
     channel = "right"
   ),
-  "list"
+  "batWav"
 )
-expect_identical(length(leislers.right$values), 0L)
+expect_identical(length(leislers.right@Wav[[1]]), 0L)

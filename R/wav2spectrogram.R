0923dcfddbc90bcfa2b492d7ffddd1d540707444
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-11-09 09:07:57 +0100

    wav2spectrogram now returns a batSpectrogram object

--- R/wav2spectrogram.R
+++ R/wav2spectrogram.R
@@ -2,6 +2,7 @@
 #' @param wav An object as created by \code{\link{read_wav}}
 #' @param window.ms The size of the window in microseconds. Default to 1.
 #' @importFrom assertthat assert_that has_name is.number
+#' @importFrom dplyr %>% filter_ select_
 #' @importFrom signal specgram
 #' @export
 #' @examples
@@ -11,23 +12,52 @@
 #'    )
 #'  )
 wav2spectrogram <- function(wav, window.ms = 1){
-  assert_that(is.list(wav))
-  assert_that(has_name(wav, "sample.rate"))
-  assert_that(has_name(wav, "values"))
-  assert_that(is.number(wav$sample.rate))
-  assert_that(is.numeric(wav$values))
-  assert_that(length(wav$values) > 0)
+  assert_that(inherits(wav, "batWav"))
+  validObject(wav)
   assert_that(is.number(window.ms))
   assert_that(window.ms > 0)
 
-  window.n <- next_power_2(wav$sample.rate * window.ms / 1000)
-  spectrogram <- specgram(
-    x = wav$values,
-    n = window.n,
-    Fs = wav$sample.rate,
-    overlap = ceiling(0.9 * window.n)
+  spectrogram <- lapply(
+    names(wav@Wav),
+    function(fingerprint){
+      if (length(wav@Wav[[fingerprint]]) == 0) {
+        return(NULL)
+      }
+      sampling.rate <- wav@Metadata %>%
+        filter_(~Fingerprint == fingerprint) %>%
+        select_(~SamplingRate) %>%
+        unlist()
+      window.n <- next_power_2(sampling.rate * window.ms / 1000)
+      spectrogram <- specgram(
+        x = wav@Wav[[fingerprint]],
+        n = window.n,
+        Fs = sampling.rate,
+        overlap = ceiling(0.9 * window.n)
+      )
+      spectrogram$S <- 20 * log10(abs(spectrogram$S))
+      spectrogram$S <- spectrogram$S - median(spectrogram$S)
+      return(spectrogram)
+    }
   )
-  spectrogram$S <- 20 * log10(abs(spectrogram$S))
-  spectrogram$S <- spectrogram$S - median(spectrogram$S)
-  return(spectrogram)
+  names(spectrogram) <- sapply(
+    names(wav@Wav),
+    function(x){
+      digest(list(x, window.ms), algo = "sha1")
+    }
+  )
+  metadata = data.frame(
+    Fingerprint = names(spectrogram),
+    File = factor(names(wav@Wav)),
+    Window = window.ms,
+    stringsAsFactors = FALSE
+  )
+  keep <- !sapply(spectrogram, is.null)
+  metadata <- metadata[keep, ]
+  spectrogram <- spectrogram[keep]
+  new(
+    "batSpectrogram",
+    Metadata = wav@Metadata,
+    SpectrogramMetadata = metadata,
+    Spectrogram = spectrogram
+  )
 }

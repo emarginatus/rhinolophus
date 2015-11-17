14e392fe9e3f00915b430ed223d8d662db1980be
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-11-17 22:14:46 +0100

    pulse_borders() return a SpatialPolygonsDataFrame with attributes on the pulse

--- R/pulse_border.R
+++ R/pulse_border.R
@@ -9,11 +9,15 @@ pulse_border <- function(pulse){
   assert_that(length(unique(pulse@PulseMetadata$Spectrogram)) == 1)
 
   base <- pulse@PulseMetadata %>%
-    select_(~Fingerprint, ~Xmin, ~Xmax, ~Ymin, ~Ymax)
+    select_(~Fingerprint, ~Xmin, ~Xmax, ~Ymin, ~Ymax) %>%
+    arrange_(~Fingerprint)
   base$Xmin <- pulse@Spectrogram[[1]]$t[base$Xmin]
   base$Xmax <- pulse@Spectrogram[[1]]$t[base$Xmax]
   base$Ymin <- pulse@Spectrogram[[1]]$f[base$Ymin]
   base$Ymax <- pulse@Spectrogram[[1]]$f[base$Ymax]
+  base <- base %>%
+    mutate_(Label = ~sprintf("%.0f ms - %.0f kHz", Xmin * 1e3, Ymin * 1e-3))
+  rownames(base) <- base$Fingerprint
 
   poly <- bind_rows(
     base %>%
@@ -32,5 +36,8 @@ pulse_border <- function(pulse){
     summarise_(poly = ~list(Polygon(coords = cbind(X, Y)))) %>%
     rowwise() %>%
     mutate_(poly = ~list(Polygons(srl = list(poly), ID = Fingerprint)))
-  SpatialPolygons(poly$poly)
+  SpatialPolygonsDataFrame(
+    SpatialPolygons(poly$poly),
+    data = base
+  )
 }

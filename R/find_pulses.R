e34a56f99c1d06ee527721a4d0b201d56d2a8036
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-05 14:54:32 +0100

    make find_pulses more efficient

--- R/find_pulses.R
+++ R/find_pulses.R
@@ -1,24 +1,33 @@
 #' Find the location of possible pulses in a spectrogram
+#'
+#' The pulses are extracted in steps of \code{delta.amplitude}. Suppose \code{delta.amplitude = 5} and \code{min.amplitude = 10}. The maximum amplitude of the current pulse is \code{max.amplitude = 43.6}. Then the pulse will be extracted with lower bounds of amplitude of \code{c(38.6, 33.6, 28.6, 23.6, 18.6, 13.6)}.
 #' @export
-#' @importFrom raster Which rowColFromCell zonal clump subs
+#' @importFrom raster clump zonal Which xyFromCell crop focal extent
 #' @importFrom assertthat assert_that is.number
 #' @importFrom dplyr %>% filter_ rename_ mutate_ summarize_ select_ inner_join rowwise
 #' @importFrom digest digest
 #' @param spectrogram The spectrogram
-#' @param min.contour The minimum amplitude of the pulse
-#' @param min.peak Take only contour into account with a maximum amplitude of at least min.peak
+#' @param min.peak Take only pulses into account with a maximum amplitude of at least min.peak
+#' @param min.amplitude The minimum amplitude of the pulse
+#' @param delta.amplitude The step size in which the pulse is extracted. See details
 #' @examples
 #'  wav <- read_wav(
 #'    system.file("demo_wav/leislers.wav", package = "rhinolophus")
 #'  )
 #'  spectrogram <- wav2spectrogram(wav)
 #'  find_pulses(spectrogram)
-find_pulses <- function(spectrogram, min.contour = 10, min.peak = 20){
-  assert_that(is.number(min.contour))
+find_pulses <- function(
+  spectrogram,
+  min.peak = 20,
+  min.amplitude = 10,
+  delta.amplitude = 5
+){
   assert_that(is.number(min.peak))
-  assert_that(min.peak > min.contour)
+  assert_that(is.number(min.amplitude))
+  assert_that(is.number(delta.amplitude))
+  assert_that(delta.amplitude > 0)
+  assert_that(min.peak > min.amplitude + delta.amplitude)
   assert_that(inherits(spectrogram, "batSpectrogram"))
-
 
   sha1 <- function(x){
     digest(x, algo = "sha1")
@@ -32,66 +41,119 @@ find_pulses <- function(spectrogram, min.contour = 10, min.peak = 20){
         spectrogram.raster <- spectrogram_raster(
           spectrogram@Spectrogram[[fingerprint]]
         )
-        delta.time <- (
-          spectrogram.raster@extent@xmax - spectrogram.raster@extent@xmin
-        ) / (ncol(spectrogram.raster) - 1)
-        delta.frequency <- (
-          spectrogram.raster@extent@ymax - spectrogram.raster@extent@ymin
-        ) / (nrow(spectrogram.raster) - 1)
-        minimum.contour <- clump(
-          spectrogram.raster >= min.contour,
+        candidate.zone <- clump(
+          spectrogram.raster > min.amplitude,
           directions = 4
         )
-        local.max <- zonal(spectrogram.raster, minimum.contour, fun = max)
-        recode <- local.max %>%
-          as.data.frame() %>%
-          filter_(~ value >= min.peak) %>%
-          rename_(AmplitudeMax = ~value) %>%
-          mutate_(Pulse = ~seq_along(zone))
-        selected.pulses <- subs(
-          minimum.contour,
-          recode,
-          by = "zone",
-          which = "Pulse"
-        )
-
+        local.max <- zonal(spectrogram.raster, candidate.zone, fun = max)
+        relevant <- local.max[
+          local.max[, 2] >= max(min.peak, min.amplitude + delta.amplitude),
+          "zone"
+        ]
         pulses <- do.call(
           rbind,
           lapply(
-            recode$Pulse,
-            function(i){
-              xy <- rowColFromCell(
-                selected.pulses,
-                Which(selected.pulses == i, cells = TRUE)
+            relevant,
+            function(zone){
+              pulse <- data.frame(
+                Xmin = numeric(0),
+                Xmax = numeric(0),
+                Ymin = numeric(0),
+                Ymax = numeric(0),
+                MaxAmplitude = numeric(0),
+                DeltaAmplitude = numeric(0)
               )
+              this.zone <- candidate.zone == zone
+              this.zonecell <- Which(this.zone, cells = TRUE)
+              xy <- xyFromCell(candidate.zone, this.zonecell)
+              this.spec <- crop(spectrogram.raster, extent(xy))
+              this.zone <- crop(this.zone, extent(xy))
+              this.spec[is.na(this.zone) | this.zone == 0] <- -Inf
+              this.max <- focal(this.spec, matrix(1, ncol = 5, nrow = 5), max)
+              candidate <- Which(
+                this.spec == this.max & is.finite(this.spec),
+                cells = TRUE
+              )
+              db <- this.spec[candidate]
+              relevant.peak <- which(db >= min.peak)
+              if (length(relevant.peak) == 0) {
+                return(pulse)
+              }
+              relevant.peak <- relevant.peak[
+                order(db[relevant.peak], decreasing = TRUE)
+              ]
+              candidate <- candidate[relevant.peak]
+              db <- db[relevant.peak]
 
-              xy %>%
-                as.data.frame() %>%
-                summarize_(
-                  Pulse = i,
-                  Xmin = ~min(col),
-                  Xmax = ~max(col),
-                  Ymin = ~as.integer(1 + nrow(spectrogram.raster) - max(row)),
-                  Ymax = ~as.integer(1 + nrow(spectrogram.raster) - min(row)),
-                  Ratio = ~n() / (diff(range(col)) * diff(range(row))),
-                  AmplitudeMin = min.contour
-                )
+              n.step <- floor(max(db) / delta.amplitude)
+              for (step in seq_len(n.step)) {
+                to.low <- which(db < (min.amplitude + step * delta.amplitude))
+                if (length(to.low) > 0) {
+                  db <- db[-to.low]
+                  candidate <- candidate[-to.low]
+                }
+                if (length(candidate) == 0) {
+                  break
+                }
+                working <- this.spec
+                i <- 1
+                while (i <= length(candidate)) {
+                  minimum.contour <- clump(
+                    working >= (db[i] - step * delta.amplitude),
+                    directions = 4
+                  )
+                  local.zone <- minimum.contour[candidate[i]]
+                  selection <- Which(
+                    minimum.contour == local.zone,
+                    cells = TRUE
+                  )
+                  same.zone <- i +
+                    which(minimum.contour[candidate[-seq_len(i)]] == local.zone)
+                  if (length(same.zone) > 0) {
+                    candidate <- candidate[-same.zone]
+                    db <- db[-same.zone]
+                  }
+                  if (
+                    any(
+                      minimum.contour[candidate[seq_len(i - 1)]] == local.zone,
+                      na.rm = TRUE
+                    )
+                  ) {
+                    candidate <- candidate[-i]
+                    db <- db[-i]
+                    next
+                  }
+                  working[selection] <- db[i] - step * delta.amplitude - 1
+                  pulse <- xyFromCell(this.spec, selection) %>%
+                    as.data.frame() %>%
+                    summarize_(
+                      Xmin = ~min(x),
+                      Xmax = ~max(x),
+                      Ymin = ~min(y),
+                      Ymax = ~max(y)
+                    ) %>%
+                    mutate_(
+                      MaxAmplitude = db[i],
+                      DeltaAmplitude = ~step * delta.amplitude
+                    ) %>%
+                    bind_rows(pulse)
+                  i <- i + 1
+                }
+              }
+              return(pulse)
             }
           )
         )
-        recode %>%
-          inner_join(x = pulses, by = "Pulse") %>%
+        pulses %>%
           mutate_(
-            Spectrogram = ~factor(fingerprint),
-            DeltaTime = delta.time,
-            DeltaFrequency = delta.frequency
+            Spectrogram = ~factor(fingerprint)
           ) %>%
-          filter_(~is.finite(Ratio)) %>%
           rowwise() %>%
           mutate_(
-            Fingerprint = ~sha1(c(Spectrogram, Xmin, Xmax, Ymin, Ymax))
+            Fingerprint = ~sha1(
+              c(Spectrogram, Xmin, Xmax, Ymin, Ymax, DeltaAmplitude)
+            )
           ) %>%
-          select_(~-zone, ~-Pulse) %>%
           as.data.frame()
       }
     )

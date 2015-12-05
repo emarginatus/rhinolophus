0f78290ecd49fe540344797dda74161400e56239
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-05 14:56:41 +0100

    update batPulse class to match new version of find_pulses

--- R/batPulse_class.R
+++ R/batPulse_class.R
@@ -17,15 +17,12 @@ setClass(
     PulseMetadata = data.frame(
       Fingerprint = character(0),
       Spectrogram = factor(character(0)),
-      Xmin = integer(0),
-      Xmax = integer(0),
-      Ymin = integer(0),
-      Ymax = integer(0),
-      DeltaTime = numeric(0),
-      DeltaFrequency = numeric(0),
-      Ratio = numeric(0),
-      AmplitudeMin = numeric(0),
-      AmplitudeMax = numeric(0),
+      Xmin = numeric(0),
+      Xmax = numeric(0),
+      Ymin = numeric(0),
+      Ymax = numeric(0),
+      MaxAmplitude = numeric(0),
+      DeltaAmplitude = numeric(0),
       stringsAsFactors = FALSE
     ),
     PulseFourier = list()
@@ -39,24 +36,18 @@ setValidity(
   function(object){
     assert_that(inherits(object@PulseMetadata$Fingerprint, "character"))
     assert_that(inherits(object@PulseMetadata$Spectrogram, "factor"))
-    assert_that(inherits(object@PulseMetadata$Xmin, "integer"))
-    assert_that(inherits(object@PulseMetadata$Xmax, "integer"))
-    assert_that(inherits(object@PulseMetadata$Ymin, "integer"))
-    assert_that(inherits(object@PulseMetadata$Ymax, "integer"))
-    assert_that(inherits(object@PulseMetadata$DeltaTime, "numeric"))
-    assert_that(inherits(object@PulseMetadata$DeltaFrequency, "numeric"))
-    assert_that(inherits(object@PulseMetadata$Ratio, "numeric"))
-    assert_that(inherits(object@PulseMetadata$AmplitudeMin, "numeric"))
-    assert_that(inherits(object@PulseMetadata$AmplitudeMax, "numeric"))
+    assert_that(inherits(object@PulseMetadata$Xmin, "numeric"))
+    assert_that(inherits(object@PulseMetadata$Xmax, "numeric"))
+    assert_that(inherits(object@PulseMetadata$Ymin, "numeric"))
+    assert_that(inherits(object@PulseMetadata$Ymax, "numeric"))
+    assert_that(inherits(object@PulseMetadata$MaxAmplitude, "numeric"))
+    assert_that(inherits(object@PulseMetadata$DeltaAmplitude, "numeric"))
 
     assert_that(anyDuplicated(object@PulseMetadata$Fingerprint) == 0)
     assert_that(anyDuplicated(names(object@PulseFourier)) == 0)
 
     assert_that(all(nchar(object@PulseMetadata$Fingerprint) == 40))
-    assert_that(all(is.finite(object@PulseMetadata$Ratio)))
-    assert_that(all(object@PulseMetadata$DeltaTime > 0))
-    assert_that(all(object@PulseMetadata$DeltaFrequency > 0))
-    assert_that(all(object@PulseMetadata$Ratio > 0))
+    assert_that(all(object@PulseMetadata$DeltaAmplitude > 0))
 
     assert_that(
       all(

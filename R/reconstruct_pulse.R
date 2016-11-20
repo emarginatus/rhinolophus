7ddf397977b0d6e246bc231220e2cfa5b0200ebc
ThierryO <Thierry.Onkelinx@inbo.be>
2016-11-20 11:11:33 +0100

    update reconstruct pulse

--- R/reconstruct_pulse.R
+++ R/reconstruct_pulse.R
@@ -1,42 +1,26 @@
 #' reconstruct a pulse
 #' @export
-#' @param pulse.fft The fourier representation of the pulse
-#' @param local.time Should the pulse be centered (TRUE) or display the original timing (FALSE)
-#' @param plot.it Should the reconstructed pulse be plotted?
-#' @param asp The aspect ratio
-#' @importFrom raster raster
-#' @importFrom graphics plot
-#' @importFrom stats fft
-reconstruct.pulse <- function(pulses, fingerprint){
+#' @param pulses The fourier representation of the pulse
+#' @param fingerprint
+#' @importFrom assertthat assert_that is.string has_name is.flag noNA
+#' @importFrom dplyr %>% filter_
+reconstruct.pulse <- function(pulses, fingerprint, plot.it = TRUE){
   assert_that(inherits(pulses, "batPulse"))
   assert_that(is.string(fingerprint))
-  assert_that(has_name(pulses@PulseFourier, fingerprint))
+  assert_that(has_name(pulses@PulseMatrix, fingerprint))
+  assert_that(is.flag(plot.it))
+  assert_that(noNA(plot.it))
 
   box <- pulses %>%
     calculate_box() %>%
     filter_(~Fingerprint == fingerprint)
-  fft_coef <- matrix(0, ncol = box$S, nrow = box$S)
-  new_coef <- pulses@PulseFourier[[fingerprint]]
-  fft_coef[seq_len(nrow(new_coef)), seq_len(ncol(new_coef))] <- new_coef
-  amplitude <- fft_coef %>%
-    fft(inverse = TRUE) %>%
-    '/'(
-      dim(pulses@PulseFourier[[fingerprint]]) %>%
-        prod()
-    ) %>%
-    abs()
-  image(amplitude)
-
-  extra.time <- ifelse(local.time, 0, pulse.fft$parameters["StartTime"])
-  puls.raster <- raster(
-    amplitude[rev(seq_len(pulse.fft$fft.dim[1])), ],
-    xmn = extra.time,
-    xmx = extra.time + pulse.fft$parameters["PulsDuration"],
-    ymn = pulse.fft$parameters["FrequencyMin"],
-    ymx = pulse.fft$parameters["FrequencyMax"]
-  )
-  if(plot.it){
-    plot(puls.raster, xlab = 'Time (ms)', ylab = "Frequency (kHz)", asp = asp)
+  puls.raster <- pulses@PulseMatrix[[fingerprint]] %>%
+    "*"(box$DeltaAmplitude) %>%
+    "+"(box$MaxAmplitude - box$DeltaAmplitude) %>%
+    as.matrix() %>%
+    raster(xmn = box$BXmin, xmx = box$BXmax, ymn = box$Ymin, ymx = box$BYmax)
+  if (plot.it) {
+    plot(puls.raster, xlab = 'Time (ms)', ylab = "Frequency (kHz)")
   }
   return(invisible(puls.raster))
 }

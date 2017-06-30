049c01675453bb76b6d7d78bba1d18c83f61b659
ThierryO <Thierry.Onkelinx@inbo.be>
2017-06-30 21:29:51 +0200

    add get_species()

---
+++ R/get_species.R
@@ -0,0 +1,58 @@
+#' get the species information of all recordings in a directory
+#' @export
+#' @importFrom dplyr data_frame %>% mutate_ select_
+#' @importFrom tidyr extract_ unnest_ spread_
+get_species <- function(path, write_csv = TRUE) {
+  species <- data_frame(
+    filename = list.files(
+      path = path,
+      pattern = "WAV$",
+      recursive = TRUE,
+      full.names = TRUE
+    )
+  ) %>%
+    mutate_(
+      timestamp = ~file.info(filename)$mtime,
+      path = ~dirname(filename),
+      filename = ~basename(filename)
+    ) %>%
+    extract_(
+      "filename",
+      into = c("tmp", "channel", "original"),
+      regex = "(.*)µ(.*)µ(.*)",
+      remove = FALSE
+    ) %>%
+    mutate_(
+      tmp = ~gsub(".*/", "", tmp) %>%
+        strsplit("_")
+    ) %>%
+    unnest_(~tmp) %>%
+    extract_(
+      "tmp",
+      into = c("species", "count"),
+      regex = "(.*)-(.*)",
+      remove = FALSE,
+      convert = TRUE
+    ) %>%
+    select_(
+      ~path, ~original, ~filename, ~channel, time_expansion = ~tmp, ~timestamp,
+      ~species, ~count
+    ) %>%
+    spread_("species", "count", fill = 0) %>%
+    mutate_(
+      time_expansion = ~gsub("[[:alpha:]]*", "", channel) %>%
+        as.integer(),
+      channel = ~gsub("[[:digit:]]*", "", channel) %>%
+        as.factor()
+    ) %>%
+    arrange_(~timestamp)
+  if (write_csv) {
+    as.data.frame(species) %>%
+    write.csv2(
+      file = paste0(path, "/determination.csv") %>%
+        normalizePath(mustWork = FALSE),
+      row.names = FALSE
+    )
+  }
+  return(species)
+}
